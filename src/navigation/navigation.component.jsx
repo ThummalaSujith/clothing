bbad1e2e2dd309f53bbd5e0b64791707a979be1a
Thummala Sujith <srt7@njit.edu>
2024-01-29 23:04:57 -0500

    added navigation logo and styles to navigation bar and implementing routing

---
+++ src/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../assests/crown.svg";
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>
+            <CrwnLogo />
+          </div>
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
